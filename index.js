1d1a7286791ae401e0aaee0f515382290803a2d7
hussein010 <abouhachemhussein@gmail.com>
2021-06-14 16:15:15 +0300

    step 5

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
 
 app.get('/', (req, res) => {
     const response={
@@ -45,10 +51,21 @@ app.get('/search', (req, res) => {
         const response= {                 
             status:500, error : true,  message:"you have to provide a search"
            };     
-           res.send(response);
-
-        
+           res.send(response);   
     }
+})
+app.get('/movies/add', (req, res) => {     
+})
+app.get('/movies/read', (req, res) => {
+    const response= {
+
+        status:200, data:movies
+    };
+    res.send(response);
+})
+app.get('/movies/edit', (req, res) => {     
+})
+app.get('/movies/delete', (req, res) => {     
 })
 
 
